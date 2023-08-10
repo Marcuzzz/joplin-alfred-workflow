97c147453c770b013838adf86ed1a3eba3d238e2
Marc Otten <marc.otten@bcnexxt.com>
2023-08-10 11:57:41 +0200

    chore(app.js): update API_TOKEN and API_BASE_URL to use environment variables for improved security and flexibility feat(app.js): add support for loading environment variables from .env file using dotenv package chore(info.plist): remove commented out code and unnecessary dialog display chore(info.plist): update script to remove unnecessary escaped ampersands chore(info.plist): update script to remove unnecessary commented out code chore(info.plist): update script to remove unnecessary commented out dialog display chore(info.plist): update script to remove unnecessary commented out code chore(info.plist): update script to remove unnecessary commented out dialog display chore(info.plist): update script to remove unnecessary commented out code chore(info.plist): update script to remove unnecessary commented out dialog display chore(info.plist): update script to remove unnecessary commented out code chore(info.plist): update script to remove unnecessary commented out dialog display chore(info.plist): update script to remove unnecessary commented

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const request = require('request');
 const { exec } = require('child_process');
+require('dotenv').config();
 
-const API_TOKEN = 'd22ec028b8493db1f2b5afbd8747d750f0d75f3107b5d67ee09253f8290f48a3e4717422cba30e361302fee8cf5181179cfa757f112e1050a4c733f6ce01544c';
-
-const API_BASE_URL = 'http://127.0.0.1:41184'; // Change this if your Joplin Web Clipper has a different port or domain.
-
-
+const API_TOKEN = process.env.joplin_key;
+const API_BASE_URL = process.env.joplin_url;
 
 function searchNotes() {
   return new Promise((resolve, reject) => {
@@ -29,40 +27,18 @@ function searchNotes() {
   });
 }
 
-// Example usage:
 searchNotes()
   .then((notes) => {
     
-    // notes.forEach((note) => {
-    //   console.log(note.title);
-    // });
-
     console.log(JSON.stringify({ "items" : notes.map((note) => ({
       title: note.title,
-      subtitle: `Paste this into a note and click on the link to open...`, // Assuming you want to show the note ID as the subtitle
+      subtitle: `Open note ...`,
       valid: true,
       arg: `[${note.title}](joplin://x-callback-url/openNote?id=${note.id})`, // Pass the note ID as an argument for Alfred to use later
       icon: {path: "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertCautionIcon.icns"}
-      // Add any other properties you want for each item, e.g., icons, etc.
     }))}));
 
   })
   .catch((err) => {
     console.error('Error:', err.message);
   });
-
-
-// exec(`open "joplin://x-callback-url/openNote?id=0abb557b91a839063a2b52bda352a193"`, (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`Error opening Joplin note: ${error.message}`);
-//     return;
-//   }
-
-//   // Output the result of the command if needed
-//   console.log(stdout);
-
-//   // If there's any error output, log it as well
-//   if (stderr) {
-//     console.error(stderr);
-//   }
-// });
